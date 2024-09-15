a458c26e0886d14837239c936ab3eaccbdd2f92d
Jaro <jaroslavfilo95@gmail.com>
2024-09-15 18:09:34 +0200

    added handling of cs -> cz

--- src/hooks/useUserLanguage.jsx
+++ src/hooks/useUserLanguage.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
 
 export default function useUserLanguage() {
-  const [language, setLanguage] = useState({});
+  const [language, setLanguage] = useState("");
 
   useEffect(() => {
     const browserLanguage = navigator.language || navigator.userLanguage;
-    setLanguage(browserLanguage);
+    const adjustedLanguage = browserLanguage === "cs" ? "cz" : browserLanguage;
+    setLanguage(adjustedLanguage);
   }, []);
 
   return language;
